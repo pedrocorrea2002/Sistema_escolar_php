f2b5cc7fcf7130a5792cbe66d1caf5e55ae6ae0c
renanrrj <david.ramos@edb.br>
2022-11-25 19:44:17 -0300

    Commit Materia

--- Componente/menu.php
+++ Componente/menu.php
@@ -2,7 +2,7 @@
         <a href="../index/form_inicial.php">Página Inicial</a>
         <a href="../aluno/form_aluno.php">Administração de alunos</a>
         <a href="../acesso/form_acesso.php">Administração de acessos</a>
-        <a href="#">Administração de materias</a>
+        <a href="../Materia/form_materia.php">Administração de materias</a>
         <a href="#">Administração de matriculas</a>
         <a href="#">Administração de notas</a>
         <a href="../Utils/proc_sessiondestroy.php">Sair</a>
---
+++ Materia/materia.php
@@ -0,0 +1,55 @@
+<?php
+require_once("../Utils/mysql.php");
+
+$sqlSelect = "select * from materia where lower(dsmateria) like '%";
+$novaMateria = "insert into materia(dsmateria) values ('@')";
+
+function selecionaMateria($nome)
+{
+    global $sqlSelect;
+    $sql = $sqlSelect . strtolower($nome) . "%'";
+
+    return selectRegistros($sql);
+}
+
+function listaMateria()
+{
+    return selectRegistros("select * from materia");
+}
+
+function existeMateria($nome)
+{
+    $retorno = selectRegistros("select * from materia where lower(dsmateria)='" . strtolower($nome) . "'");
+
+    if (count($retorno) > 0) return true;
+    else return false;
+}
+function cadastrarMateria($nome)
+{
+    global $novaMateria;
+    $sql = str_replace("@",$nome,$novaMateria);
+
+    return insereRegistro($sql);
+}
+
+function getName($id)
+{
+    $retorno = selectRegistros("select * from materia where idmateria='" . $id . "'");
+
+    return $retorno[0]['dsmateria'];
+}
+
+function setName($id, $nome)
+{
+    $sql = "UPDATE MATERIA SET dsmateria='" . $nome . "' WHERE idmateria=" . $id;
+
+    return updateRegistro($sql);
+}
+function delID($id)
+{
+    $sql = "DELETE FROM MATERIA  WHERE idmateria=" . $id;   
+
+    return deleteRegistro($sql);
+}
+
+?>
