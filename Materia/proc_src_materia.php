3971615879f22e1312bdf21534d21585d4afa366
pedrocorrea2002 <pedro.hcorrea15@gmail.com>
2022-12-03 01:57:58 -0300

    atualização faltante

--- Materia/proc_src_materia.php
+++ Materia/proc_src_materia.php
@@ -2,7 +2,6 @@
 require_once('../Componente/header.php'); //* ESSE HEADER ESTÁ SENDO CHAMADO SÓ PARA PEGAR dslogin DO USUÁRIO LOGADO
 require_once('../Utils/mysql.php');
 require_once('../Utils/valida_formulario.php');
-<<<<<<< HEAD
 require_once('../Materia/materia.php');
 
 if(isset($_POST['srcMateria']) && !empty(trim($_POST['srcMateria']))){
@@ -11,19 +10,6 @@ if(isset($_POST['srcMateria']) && !empty(trim($_POST['srcMateria']))){
     //* VERIFICANDO A PRESENÇA DE CÓDIGO MALICIOSO DENTRO DO srcAluno
     if(!caracteresInvalidos($srcMateria)){
         header("Location: form_Materia.php?filter=$srcMateria");
-=======
-require_once('materia.php');
-
-echo "fora: ".$_POST['srcMateria'];
-
-if(isset($_POST['srcMateria']) && !empty(trim($_POST['srcMateria']))){
-    $srcMateria = trim($_POST['srcMateria']);
-    $dslogin = $_SESSION['dslogin'];
-
-    //* VERIFICANDO A PRESENÇA DE CÓDIGO MALICIOSO DENTRO DO srcAluno
-    if(!caracteresInvalidos($srcMateria)){
-        header("Location: form_materia.php?filter=$srcMateria");
->>>>>>> 8e00485073b29e7c50c2370f420a4644edbeada3
     }
 }else{
     header("Location: form_materia.php");
