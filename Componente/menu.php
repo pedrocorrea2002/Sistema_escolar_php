631a4d816c3551403a2470bc2d14c37afb1d9e09
pedrocorrea2002 <pedro.hcorrea15@gmail.com>
2023-01-21 19:21:03 -0300

    Adição do nome do usuário em bem vindo

--- Componente/menu.php
+++ Componente/menu.php
@@ -1,11 +1,13 @@
 <?php?>
-<div class="sidenav">
-        <a href="../index/form_inicial.php"><img src="../Imagens/pagina_inicial.png"/>Página Inicial</a>
-        <a href="../aluno/form_aluno.php"><img src="../Imagens/admin_alunos.png"/>Administração de alunos</a>
-        <a href="../acesso/form_acesso.php"><img src="../Imagens/admin_acessos.png"/>Administração de acessos</a>
-        <a href="../Materia/form_materia.php"><img src="../Imagens/admin_materias.png"/>Administração de materias</a>
-        <a href="../matricula/form_matricula.php"><img src="../Imagens/admin_matriculas.png"/>Administração de matriculas</a>
-        <a href="../Avaliacao/form_avaliacao.php"><img src="../Imagens/admin_avaliacoes.png"/>Administração de avaliacões</a>
-        <a href="../notas/form_notas.php"><img src="../Imagens/admin_notas.png"/>Administração de notas</a>
-        <a href="../Utils/proc_sessiondestroy.php"><img src="../Imagens/sair.png"/>Sair</a>
+<div class="sidenav"> 
+        <div class="pages_menu">
+                <a href="../index/form_inicial.php"><img src="../Imagens/pagina_inicial.png"/>Página Inicial</a>
+                <a href="../aluno/form_aluno.php"><img src="../Imagens/admin_alunos.png"/>Administração de alunos</a>
+                <a href="../acesso/form_acesso.php"><img src="../Imagens/admin_acessos.png"/>Administração de acessos</a>
+                <a href="../Materia/form_materia.php"><img src="../Imagens/admin_materias.png"/>Administração de materias</a>
+                <a href="../matricula/form_matricula.php"><img src="../Imagens/admin_matriculas.png"/>Administração de matriculas</a>
+                <a href="../Avaliacao/form_avaliacao.php"><img src="../Imagens/admin_avaliacoes.png"/>Administração de avaliacões</a>
+                <a href="../notas/form_notas.php"><img src="../Imagens/admin_notas.png"/>Administração de notas</a>
+        </div>
+        <a class="logout_button" href="../Utils/proc_sessiondestroy.php"><img src="../Imagens/sair.png"/>Sair</a>
 </div>
--- Index/form_inicial.php
+++ Index/form_inicial.php
@@ -1,10 +1,18 @@
-<?php require_once("../Componente/header.php") ?>
+<?php 
+    require_once("../Componente/header.php");
+    $login = trim($_SESSION['dslogin']);
+
+    $sql_nmaluno = "SELECT a.nmaluno FROM login l INNER JOIN aluno a ON a.idaluno = l.idaluno WHERE l.dslogin = '$login'";
+
+    $nmaluno = selectRegistros($sql_nmaluno)[0];
+    $nmaluno = explode(' ',$nmaluno['nmaluno'])[0];
+?>
 <body>
 
 <?php require_once("../Componente/menu.php") ?>
 
     <div class="content">
-        <h2>Página inicial. Bem vindo!</h2>
+        <h2>Página inicial. Bem vindo <?php echo $nmaluno?>!</h2>
         <p>Escolha uma opção no menu a esquerda</p>
     </div>
 </body>
